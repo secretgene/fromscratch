59cd8e4507f3fc878ac91a410bf63c8c5ae94bda
sreevinayak1999 <35804500+sreevinayak1999@users.noreply.github.com>
2018-10-28 08:46:21 +0530

    new change2

--- Random_forest_implmentation.py
+++ Random_forest_implmentation.py
@@ -31,16 +31,16 @@ def str_column_to_int(dataset, column):
 		row[column] = lookup[row[column]]
 	return lookup
 
-# Split a dataset into k folds
+    # Split a dataset into k folds
 def cross_validation_split(dataset, n_folds):
-	dataset_split = list()
-	dataset_copy = list(dataset)
-	fold_size = int(len(dataset) / n_folds)
-	for i in range(n_folds):
-		fold = list()
-		while len(fold) < fold_size:
-			index = randrange(len(dataset_copy))
-			fold.append(dataset_copy.pop(index))
+	 dataset_split = list()
+	 dataset_copy = list(dataset)
+	 fold_size = int(len(dataset) / n_folds)
+	 for i in range(n_folds):
+		 fold = list()
+		 while len(fold) < fold_size:
+			 index = randrange(len(dataset_copy))
+			 fold.append(dataset_copy.pop(index))
 		dataset_split.append(fold)
 	return dataset_split
 
